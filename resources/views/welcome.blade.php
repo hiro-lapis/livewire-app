79c07f0ab0179e7efa9cf1e3f2079f7c13dc3b11
hiro-lapis <coolish555ddddd@gmail.com>
2020-11-17 23:58:57 +0900

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,6 @@
                 font-family: 'Nunito';
             }
         </style>
-        @livewireStyles
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -129,6 +128,5 @@
                 </div>
             </div>
         </div>
-        @livewireScripts
     </body>
 </html>
